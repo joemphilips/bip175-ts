166be29472b13afdb0749c62568b548631d403d5
joemphilips <joemphilips@gmail.com>
2018-03-11 23:47:23 +0900

    show significant progress

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import * as bitcoin from 'bitcoinjs-lib'
+import {HDNode} from "bitcoinjs-lib";
 
-let paymentBase = bitcoin.HDNode()
 
-export default class P2CHWallet {
+export default class P2CH {
+  public contractBase: HDNode;
+  constructor(public docs: string[], private k: number=0, master: string){
+     this.contractBase = HDNode.fromBase58(master).derivePath(`m/175'/${k}'`)
+  }
 
+
+  public derive() {
+    const hashes = this.docs.map(d => bitcoin.crypto.sha256(Buffer.from(d))).sort()
+    this.contractBase.getPublicKeyBuffer().toString()
+  };
 }
--- test/index.ts
+++ test/index.ts
@@ -1,2 +1,31 @@
-import P2CHWallet from './src'
-import ava from 'ava'
+import P2CH from '../src'
+import test from 'ava'
+import * as bitcoin from 'bitcoinjs-lib'
+const tv = require("./fixtures")
+
+tv.valid.forEach((suite: any) => {
+  let docs: Array<string> = [];
+  let derivedDocHash: Buffer[] = [];
+
+  test("Hash derived from documents", t => {
+    Object.keys(suite.docs).forEach((k, i) => {
+      docs.push(k);
+      derivedDocHash.push(bitcoin.crypto.sha256(Buffer.from(k)))
+      t.deepEqual(derivedDocHash[i], Buffer.from(suite.docs[k], 'hex'))
+    });
+  });
+
+  test("can make xpubContractBase", t => {
+    if (!suite.xpubContractBase || !suite.master) {
+      t.pass()
+      return;
+    }
+    const docHashSorted = derivedDocHash.map(h => h.toString('hex')).sort();
+
+    let master = suite.master;
+    let p2ch = new P2CH(docs, 0, master);
+    let hashConcatenated = p2ch.contractBase.neutered().toBase58()+
+      docHashSorted.reduce((a: string, b: string) => a + b );
+    t.deepEqual(suite.hashConcat, hashConcatenated);
+  })
+});
